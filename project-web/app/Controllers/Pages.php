59cbc47cfc397150a05b6697ccc3af8f8edc868b
carolay02 <100611561+carolay02@users.noreply.github.com>
2022-03-08 23:16:49 -0600

    Update Pages.php

--- project-web/app/Controllers/Pages.php
+++ project-web/app/Controllers/Pages.php
@@ -11,7 +11,16 @@ class Pages extends BaseController
 
     public function view($page = 'home')
     {
-        // ...
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+        // Whoops, we don't have a page for that!
+        throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+    }
+
+    $data['title'] = ucfirst($page); // Capitalize the first letter
+
+    echo view('header', $data);
+    echo view('pages/' . $page, $data);
+    echo view('footer', $data);
     }
 }
 
