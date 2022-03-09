f27085a51968c862abc92b805e092df40075204f
carolay02 <100611561+carolay02@users.noreply.github.com>
2022-03-08 23:15:18 -0600

    Update about.php

--- project-web/app/Views/about.php
+++ project-web/app/Views/about.php
@@ -7,18 +7,3 @@
     <h1>Sobre nosotros</h1>
   </body>
 </html>
-<?php   
-public function view($page = 'home')
-{
-    if (! is_file(APPPATH . 'Views/' . $page . '.php')) {
-        // Whoops, we don't have a page for that!
-        throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
-    }
-
-    $data['title'] = ucfirst($page); // Capitalize the first letter
-
-    echo view('header', $data);
-    echo view('pages/' . $page, $data);
-    echo view('footer', $data);
-}
-?>
